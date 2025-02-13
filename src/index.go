79e3e487b81f8d6d3a26bbc09b11431491d9d39f
Kelly <wuxuw0830@gmail.com>
2025-02-13 15:43:13 +0800

    [Modify]-Adjust router for using docker

--- src/index.go
+++ src/index.go
@@ -1,44 +1,50 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "io/ioutil"
-    "os"
-    "github.com/gin-gonic/gin"
-    "reflect"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"reflect"
+
+	"github.com/gin-gonic/gin"
 )
-func main(){
-	    jsonFile, err := os.Open("db.json")
-    if err != nil {
-        fmt.Println(err)
-    }
-    defer jsonFile.Close()
+
+func main() {
+	jsonFile, err := os.Open("db.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
-    
+
 	var result map[string]any
 
-    json.Unmarshal([]byte(byteValue), &result)
+	json.Unmarshal([]byte(byteValue), &result)
 
-	router :=gin.Default()
-    //set Https TSL
-    //router := gin.Default()
-    //go router.RunTLS(":443", config.SSL.CertFile, config.SSL.CertKey)
-    //User Login
+	router := gin.Default()
+	//set Https TSL
+	//router := gin.Default()
+	//go router.RunTLS(":443", config.SSL.CertFile, config.SSL.CertKey)
+	//User Login
 	userLogin := result["USERLOGIN"].(map[string]any)
 	fmt.Println(reflect.TypeOf(userLogin))
-    //User Information
-    userInfo := result["USERINFO"].(map[string]any)
+	//User Information
+	userInfo := result["USERINFO"].(map[string]any)
 
-    router.POST("/userLogin", func(c *gin.Context) {c.JSON(200,userLogin)})
-	
-    router.GET("/userInfo", func(c *gin.Context) {c.JSON(200,userInfo)})
+	router.POST("/userLogin", func(c *gin.Context) { c.JSON(200, userLogin) })
 
-    router.GET("/getImage", func(c *gin.Context) {c.File("./image/Img_debit_card.jpg")})
+	router.GET("/userInfo", func(c *gin.Context) { c.JSON(200, userInfo) })
 
-    router.Run("localhost:3001")
+	router.GET("/getImage", func(c *gin.Context) { c.File("./image/Img_debit_card.jpg") })
+	//using go run index.go
+	//router.Run("localhost:3001")
+
+	//using docker
+	router.Run(":3001")
 }
+
 /*
 *Post JSON by local file
-*/
+ */
